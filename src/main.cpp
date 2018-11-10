5a806555149f8765a4782f74ec03062a70efb9bc
mjakobczyk <m.jakob@wp.pl>
2018-11-10 23:17:42 +0100

    Change directories structure

---
+++ src/main.cpp
@@ -0,0 +1,15 @@
+// #include "basic.h"
+#include <iostream>
+#include "SFML/Graphics.hpp"
+
+int main()
+{
+	//Game game1;
+	//game1.run();
+	
+	std::cout << "Hello world!" << std::endl;
+
+	sf::Clock ck;
+
+	return 0;
+}
