a0381379904d73bc7235fe34646735cf45baffb6
mjakobczyk <michal.jakobczyk@protonmail.com>
2021-02-14 20:57:47 +0100

    Introduce Constants class with static accessors

---
+++ include/constants.h
@@ -0,0 +1,13 @@
+#ifndef CONSTANTS_H_
+#define CONSTANTS_H_
+
+// Constants hold useful information about game such as FPS, window height or width.
+class Constants
+{
+public:
+    static unsigned short FPS();
+    static unsigned short height();
+    static unsigned short width();
+};
+
+#endif
